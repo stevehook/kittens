d847c8e441fcc019d7c6f9bb38da8e7e6a4a4e45
Steve Hook <steve.hook@gmail.com>
2019-03-10 22:54:32 +0000

    Plug in the location API to the KittenFinder

--- lib/kitten_finder.rb
+++ lib/kitten_finder.rb
@@ -18,7 +18,7 @@ class KittenFinder
 
   EMAIL = 'steve.hook@gmail.com'
   DIRECTIONS_URL = "http://which-technical-exercise.herokuapp.com/api/#{EMAIL}/directions"
-  LOCATION_URL = "http://which-technical-exercise.herokuapp.com/api/#{EMAIL}/location/:x/:y"
+  LOCATION_URL = "http://which-technical-exercise.herokuapp.com/api/#{EMAIL}/location"
   SUCCESS_MESSAGE = "Congratulations! The search party successfully recovered the missing kittens. Please zip up your code and send it to richard.hart@which.co.uk"
 
   def fetch_directions
@@ -33,14 +33,13 @@ class KittenFinder
   end
 
   def send_location(x, y)
-    # uri = URI.parse(LOCATION_URL)
-    # response = Net::HTTP.get_response(uri)
-    # if response.code == '200'
-    #   results = JSON.parse(response.body)
-    #   results['message']
-    # else
-    #   raise "Location API returned a non-success code #{response.code}".to_s
-    # end
-    SUCCESS_MESSAGE
+    uri = URI.parse("#{LOCATION_URL}/#{x}/#{y}")
+    response = Net::HTTP.get_response(uri)
+    if response.code == '200'
+      results = JSON.parse(response.body)
+      results['message']
+    else
+      raise "Location API returned a non-success code #{response.code}".to_s
+    end
   end
 end
--- spec/kitten_finder_spec.rb
+++ spec/kitten_finder_spec.rb
@@ -8,6 +8,8 @@ RSpec.describe KittenFinder do
   let(:location_response) { '{"message":"Congratulations! The search party successfully recovered the missing kittens. Please zip up your code and send it to richard.hart@which.co.uk"}' }
   let(:directions_response_code) { 200 }
   let(:location_response_code) { 200 }
+  let(:x) { 5 }
+  let(:y) { 2 }
 
   before do
     stub_request(
@@ -25,11 +27,46 @@ RSpec.describe KittenFinder do
       body: directions_response,
       headers: {}
     )
+
+    stub_request(
+      :get,
+      "#{KittenFinder::LOCATION_URL}/#{x}/#{y}"
+    ).with(
+      headers: {
+        'Accept' => '*/*',
+        'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+        'Host' => 'which-technical-exercise.herokuapp.com',
+        'User-Agent' => 'Ruby'
+      }
+    ).to_return(
+      status: location_response_code,
+      body: location_response,
+      headers: {}
+    )
   end
 
   context 'when directions API returns valid directions' do
-    it 'returns a success' do
-      expect(subject.find).to eql KittenFinder::SUCCESS_MESSAGE
+    context 'when location API returns a successful search message' do
+      it 'returns a success' do
+        expect(subject.find).to eql KittenFinder::SUCCESS_MESSAGE
+      end
+    end
+
+    context 'when location API returns a 500 error' do
+      let(:location_response) { '' }
+      let(:location_response_code) { 500 }
+
+      it 'raises an exception' do
+        expect{ subject.find }.to raise_error 'Location API returned a non-success code 500'
+      end
+    end
+
+    context 'when location API returns a failed search message' do
+      let(:location_response) { '{"message":"Unfortunately, the search party failed to recover the missing kittens. You have 4 attempts remaining."}' }
+
+      it 'returns a failure message' do
+        expect(subject.find).to eql 'Unfortunately, the search party failed to recover the missing kittens. You have 4 attempts remaining.'
+      end
     end
   end
 
