ea77c32da918aab4342090d2ca51fb00029484b4
Steve Hook <steve.hook@gmail.com>
2019-03-10 21:54:28 +0000

    Outline the rest of the KittenFinder#find method

--- lib/kitten_finder.rb
+++ lib/kitten_finder.rb
@@ -5,17 +5,23 @@ require 'json'
 
 class KittenFinder
   def find
-    # TODO:
     # 1. Fetch instructions from `/api/:email/directions`
     directions = fetch_directions
+
     # 2. Calculate target coordinates
+    x, y = LocationFinder.new(directions).coordinates
+
     # 3. Call /api/:email/location/:x/:y to send search party
-    # 3. Return success or failure
+    search_response = send_location(x, y)
+
+    # 4. Return success or failure
+    # TODO Parse the response
     true
   end
 
   EMAIL = 'steve.hook@gmail.com'
   DIRECTIONS_URL = "http://which-technical-exercise.herokuapp.com/api/#{EMAIL}/directions"
+  LOCATION_URL = "http://which-technical-exercise.herokuapp.com/api/#{EMAIL}/location/:x/:y"
 
   def fetch_directions
     uri = URI.parse(DIRECTIONS_URL)
@@ -26,5 +32,17 @@ class KittenFinder
     else
       raise "Directions API returned a non-success code #{response.code}".to_s
     end
+  end
+
+  def send_location(x, y)
+    # uri = URI.parse(LOCATION_URL)
+    # response = Net::HTTP.get_response(uri)
+    # if response.code == '200'
+    #   results = JSON.parse(response.body)
+    #   # TODO: Find out what format the response is in
+    # else
+    #   raise "Location API returned a non-success code #{response.code}".to_s
+    # end
+    {}
   end
 end
