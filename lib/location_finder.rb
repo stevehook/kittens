feb698ec9be4fa3447c0232dfe446de881b563bc
Steve Hook <steve.hook@gmail.com>
2019-03-10 22:22:14 +0000

    Basic implementation for turning right

--- lib/location_finder.rb
+++ lib/location_finder.rb
@@ -24,12 +24,26 @@ class LocationFinder
   def apply_direction(direction)
     if direction == FORWARD
       move_forward
+    elsif direction == RIGHT
+      turn_right
     end
   end
 
   def move_forward
     [0, 1].each do |n|
       self.coordinates[n] = self.coordinates[n] + self.heading[n]
+    end
+  end
+
+  def turn_right
+    if heading == [0, 1]
+      self.heading = [1, 0]
+    elsif heading == [1, 0]
+      self.heading = [0, -1]
+    elsif heading == [0, -1]
+      self.heading = [-1, 0]
+    else
+      self.heading == [0, 1]
     end
   end
 end
